beea1b5943784b57ae1f89d7cc845a2046076f31
denys-zavhorodnii <dzavhorodnii@kse.org.ua>
2024-10-15 21:21:00 +0300

    update

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-const API_KEY = "****";
+const API_KEY = "";
 
 
-fetch('https://api.thecatapi.com/v1/breeds?api_key=' + API_KEY).then(res => res.json()).then((data) => {
+const params = new URLSearchParams();
+params.set("api_key", API_KEY);
+
+fetch('https://api.thecatapi.com/v1/breeds?' + params.toString(), {
+    headers: new Headers({
+        'x-api-key': API_KEY
+    })
+}).then(res => res.json()).then((data) => {
     const elements = data.map(item => {
         const { description, name, image: { url } = {url: null}} = item;
         return render(url, name, description);
     });
 
     const container = document.body.querySelector('.cat-list');
-    // container.innerHTML = elements.join("");
+    container.innerHTML = elements.join("");
 })
 
 function render(img, name, description) {
